8d826e3d72932194f7a29b9ff32d9f02c5b6c037
uqe <arturkkos@gmail.com>
2023-05-01 01:48:07 +0300

    upd deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,13 +1,13 @@
-import ytdl from "https://deno.land/x/ytdl_core/mod.ts";
+import ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
 import ffmpegInstaller from "npm:@ffmpeg-installer/ffmpeg";
 import ffmpeg from "npm:fluent-ffmpeg";
 
-export { exists } from "https://deno.land/std/fs/mod.ts";
-export { config } from "https://deno.land/x/dotenv/mod.ts";
+export { exists } from "https://deno.land/std@0.185.0/fs/mod.ts";
+export { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 export { Bot } from "https://deno.land/x/grammy@v1.15.3/mod.ts";
 export type { Message } from "https://deno.land/x/grammy_types@v3.0.3/message.ts";
-export { DB } from "https://deno.land/x/sqlite/mod.ts";
-export { getInfo } from "https://deno.land/x/ytdl_core/mod.ts";
+export { DB } from "https://deno.land/x/sqlite@v3.7.2/mod.ts";
+export { getInfo } from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
 export type { VideoInfo } from "https://deno.land/x/ytdl_core@v0.1.2/src/types.ts";
 export * as fs from "node:fs";
 export { Podcast } from "npm:podcast";
