12c3d8fb54c2e05c30e1c16fe166968705f587c6
uqe <arturkkos@gmail.com>
2023-06-28 02:55:26 +0300

    use deno ffmpeg instead of node's

--- src/convert-video.ts
+++ src/convert-video.ts
@@ -1,8 +1,6 @@
 import { addVideoToDb } from "./db.ts";
-import { exists, ffmpeg, ffmpegInstaller, Message, VideoInfo } from "./deps.ts";
+import { exists, FfmpegClass, Message, VideoInfo } from "./deps.ts";
 import { generateFeed } from "./generate-feed.ts";
-
-ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 const updateDb = async (info: VideoInfo) => {
   await addVideoToDb(
@@ -16,24 +14,26 @@ const updateDb = async (info: VideoInfo) => {
   );
 };
 
-export const convertVideo = (info: VideoInfo, handler?: (text: string) => Promise<Message.TextMessage>) => {
+export const convertVideo = async (info: VideoInfo, handler?: (text: string) => Promise<Message.TextMessage>) => {
   const { videoId } = info.videoDetails;
 
-  ffmpeg(`./public/files/${videoId}.mp4`)
-    .audioBitrate(128)
-    .save(`./public/files/${videoId}.mp3`)
-    .on("start", () => console.log("Conversion started!"))
-    .on("end", async () => {
-      console.log("Conversion ended successfully");
-      updateDb(info);
+  const ffmpeg = new FfmpegClass({
+    input: `./public/files/${videoId}.mp4`,
+    // ffmpegDir: "/opt/homebrew/bin/ffmpeg"
+    ffmpegDir: "ffmpeg",
+  });
 
-      if (await exists(`./public/files/${videoId}.mp4`)) {
-        Deno.remove(`./public/files/${videoId}.mp4`);
-      }
+  console.log("Conversion started!");
+  await ffmpeg.audioBitrate(128).save(`./public/files/${videoId}.mp3`);
+  console.log("Conversion ended successfully");
+  await updateDb(info);
 
-      console.log("Start regenerating RSS feed");
-      generateFeed();
-      console.log("Feed regenerated successfully");
-      handler && handler("RSS feed was successfully updated.");
-    });
+  if (await exists(`./public/files/${videoId}.mp4`)) {
+    Deno.remove(`./public/files/${videoId}.mp4`);
+  }
+
+  console.log("Start regenerating RSS feed");
+  generateFeed();
+  console.log("Feed regenerated successfully");
+  handler && handler("RSS feed was successfully updated.");
 };
--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,5 @@
 import ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
-import ffmpegInstaller from "npm:@ffmpeg-installer/ffmpeg";
-import ffmpeg from "npm:fluent-ffmpeg";
+import { FfmpegClass } from "https://deno.land/x/deno_ffmpeg@v3.1.0/mod.ts";
 import { Podcast } from "npm:podcast";
 
 export type { Message } from "https://deno.land/x/grammy_types@v3.0.3/message.ts";
@@ -12,4 +11,4 @@ export { Bot } from "https://deno.land/x/grammy@v1.15.3/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.7.2/mod.ts";
 export { getInfo } from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
 
-export { ffmpeg, ffmpegInstaller, Podcast, ytdl };
+export { FfmpegClass, Podcast, ytdl };
