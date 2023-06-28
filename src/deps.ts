dfa188006ed0b50be4f8f722ae93a1598f1c016d
uqe <arturkkos@gmail.com>
2023-06-28 04:43:25 +0300

    use esm.sh for `podcast` dep

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-import ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
 import { FfmpegClass } from "https://deno.land/x/deno_ffmpeg@v3.1.0/mod.ts";
-import { Podcast } from "npm:podcast";
+import ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
+import { Podcast } from "https://esm.sh/podcast@2.0.1";
 
 export type { Message } from "https://deno.land/x/grammy_types@v3.0.3/message.ts";
 export type { VideoInfo } from "https://deno.land/x/ytdl_core@v0.1.2/src/types.ts";
