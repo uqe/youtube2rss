417c3e5d428a93e123dffca217614c27938abe88
uqe <arturkkos@gmail.com>
2023-08-14 04:02:12 +0300

    fix `build-rss` task

--- src/build-rss.ts
+++ src/build-rss.ts
@@ -1,6 +1,11 @@
 import { getAllVideos } from "./db.ts";
 import { generateFeed } from "./generate-feed.ts";
 
+if (!Deno.env.get("SERVER_URL")) {
+  console.error("SERVER_URL variable is missing.");
+  Deno.exit(1);
+}
+
 console.log("Building RSS feed...");
 generateFeed(getAllVideos());
 console.log("RSS feed built.");
