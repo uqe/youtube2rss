5acc787be1ee13dda9d7440507908dab813c6a92
uqe <arturkkos@gmail.com>
2023-10-22 23:55:59 +0300

    upd deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,18 +1,18 @@
 import { FfmpegClass } from "https://deno.land/x/deno_ffmpeg@v3.1.0/mod.ts";
-import { S3Client } from "https://deno.land/x/s3_lite_client@0.6.1/mod.ts";
-import { parse } from "https://deno.land/x/xml@2.1.1/mod.ts";
+import { S3Client } from "https://deno.land/x/s3_lite_client@0.6.2/mod.ts";
+import { parse } from "https://deno.land/x/xml@2.1.2/mod.ts";
 import ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
 import { Podcast } from "https://esm.sh/podcast@2.0.1";
 
 export type { Message } from "https://deno.land/x/grammy_types@v3.3.0/message.ts";
 export type { VideoInfo } from "https://deno.land/x/ytdl_core@v0.1.2/src/types.ts";
 
-export { exists, existsSync } from "https://deno.land/std@0.203.0/fs/mod.ts";
-export { Bot } from "https://deno.land/x/grammy@v1.19.0/mod.ts";
+export { exists, existsSync } from "https://deno.land/std@0.204.0/fs/mod.ts";
+export { Bot } from "https://deno.land/x/grammy@v1.19.2/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
 export { getInfo } from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
 
-export { assertEquals, assertObjectMatch } from "https://deno.land/std@0.203.0/assert/mod.ts";
-export { afterAll, afterEach, beforeAll, describe, it } from "https://deno.land/std@0.203.0/testing/bdd.ts";
+export { assertEquals, assertObjectMatch } from "https://deno.land/std@0.204.0/assert/mod.ts";
+export { afterAll, afterEach, beforeAll, describe, it } from "https://deno.land/std@0.204.0/testing/bdd.ts";
 
 export { FfmpegClass, parse, Podcast, S3Client, ytdl };
